bb617a5538afcd77d3adfb847a26058d758ee078
inowen <inoah@correo.ugr.es>
2020-08-12 12:23:37 +0200

    Temporary fix to whitelist.
    The whitelist names were supposed to be read from a txt file in the mod's resource folder, but creating that folder seems to make the game crash when loading SkyBot in Forge. Until this is fixed, the names for the whitelist will be those that are detected at the moment of activation of the whitelist mod.

--- moduleSystem/mods/WhiteList.java
+++ moduleSystem/mods/WhiteList.java
@@ -1,44 +1,38 @@
 package inowen.moduleSystem.mods;
 
 
-import inowen.SkyBotMod;
 import inowen.moduleSystem.Module;
 import inowen.utils.ForgeKeys;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
  * Check the player entities around, if one that isn't on the whitelist is detected,
  * shut off all bots.
+ * //TODO: Change it so that it reads the names from a whitelist.txt file in src/resources/SkyBotAutomation/assets
+ * Provisionally, just take the list of players detected at the moment when the mod was enabled as whitelist.
  */
 public class WhiteList extends Module {
 
-    private static ArrayList<String> whitelistedUsers; // List of users that don't cause the bot to shut off.
-
-    // Get the whitelisted usernames from the assets folder in Resources for this mod
-
-    static {
-        whitelistedUsers = new ArrayList<String>();
-        InputStream stream = SkyBotMod.class.getResourceAsStream("assets/SkyBotMod");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-        try {
-            while(reader.ready()) {
-                whitelistedUsers.add(reader.readLine());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    private ArrayList<String> whitelistedUsers; // List of users that don't cause the bot to shut off.
 
 
     public WhiteList() {
         super("WhiteList", ForgeKeys.KEY_O);
+    }
+
+    @Override
+    public void onEnable() {
+        whitelistedUsers = new ArrayList<String>();
+        Iterable<Entity> allEntities = mc.world.getAllEntities();
+        for (Entity entity : allEntities) {
+            if (entity instanceof PlayerEntity) {
+                String playerName = ((PlayerEntity) entity).getName().getString();
+                whitelistedUsers.add(playerName);
+            }
+        }
     }
 
     @Override
@@ -53,6 +47,7 @@ public class WhiteList extends Module {
                 for (String name : whitelistedUsers) {
                     System.out.println(name);
                 }
+                // Testing done.
             }
         }
     }
