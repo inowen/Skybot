e8f4711a922073985f5b251477c9e06f8db08130
inowen <inoah@correo.ugr.es>
2020-08-12 20:59:31 +0200

    Whitelist now reads names from file.
    Whitelist now reads the whitelisted player usernames from whitelist.txt in this mods's resources folder.

--- moduleSystem/mods/WhiteList.java
+++ moduleSystem/mods/WhiteList.java
@@ -1,19 +1,25 @@
 package inowen.moduleSystem.mods;
 
 
+import inowen.SkyBotMod;
 import inowen.moduleSystem.Module;
 import inowen.moduleSystem.ModuleManager;
 import inowen.utils.ForgeKeys;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ResourceLocation;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 /**
  * Check the player entities around, if one that isn't on the whitelist is detected,
  * shut off all bots.
- * //TODO: Change it so that it reads the names from a whitelist.txt file in src/resources/SkyBotAutomation/assets
- * Provisionally, just take the list of players detected at the moment when the mod was enabled as whitelist.
+ * The list of whitelisted players is in src/main/resources/assets/skybot/whitelist.txt.
  */
 public class WhiteList extends Module {
 
@@ -27,13 +33,22 @@ public class WhiteList extends Module {
     @Override
     public void onEnable() {
         whitelistedUsers = new ArrayList<String>();
-        Iterable<Entity> allEntities = mc.world.getAllEntities();
-        for (Entity entity : allEntities) {
-            if (entity instanceof PlayerEntity) {
-                String playerName = ((PlayerEntity) entity).getName().getString();
-                whitelistedUsers.add(playerName);
+
+        // Get the whitelist file from resources
+        ResourceLocation resLoc = new ResourceLocation(SkyBotMod.MOD_ID, "whitelist.txt");
+        try {
+            InputStream stream = Minecraft.getInstance().getResourceManager().getResource(resLoc).getInputStream();
+            DataInputStream dataInputStream = new DataInputStream(stream);
+            String line = dataInputStream.readLine();
+            while(line != null) {
+                whitelistedUsers.add(line);
+                line = dataInputStream.readLine();
             }
+        } catch(Exception e) {
+            System.out.println("Problem with src/main/resources/assets/"+SkyBotMod.MOD_ID+"/whitelist.txt file. Could not get whitelisted names.");
+            whitelistedUsers.add(mc.getSession().getUsername());
         }
+
     }
 
     @Override
