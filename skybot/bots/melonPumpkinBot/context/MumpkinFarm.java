94f9003e9e3f9e83f07f764007fbfa05c2a2214e
inowen <inoah@correo.ugr.es>
2020-08-15 17:49:48 +0200

    Method to update content of FarmSlots matrix

--- skybot/bots/melonPumpkinBot/context/MumpkinFarm.java
+++ skybot/bots/melonPumpkinBot/context/MumpkinFarm.java
@@ -1,8 +1,7 @@
 package inowen.skybot.bots.melonPumpkinBot.context;
 
 import inowen.skybot.utils.FarmZoneConstraints;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.math.BlockPos;
 
@@ -25,13 +24,62 @@ public class MumpkinFarm {
      * Load the farm in. Stop if no farm found.
      */
     public void init(MumpkinInitTracker tracker) {
+        // Find the farm borders.
         zoneConstraints = new FarmZoneConstraints(BARRIER_BLOCK);
         zoneConstraints.findCorners(new BlockPos(mc.player.getPositionVector()), tracker);
+
+        if (tracker.foundFarmConstraints) {
+            // Create the FarmSlot matrix (x, z)
+            farmSlots = new FarmSlot[zoneConstraints.lengthXAxis()][zoneConstraints.lengthZAxis()];
+            // Get the contents of those farmslots for the first time.
+            updateFarmSlots();
+        }
 
 
     }
 
+    /**
+     * Update all information in the farm to use it in this tick.
+     */
     public void update() {
+        if(mc.player != null && mc.world != null) {
+            updateFarmSlots();
+        }
+    }
+
+    /**
+     * Recalculate content for the FarmSlot matrix.
+     */
+    public void updateFarmSlots() {
+        BlockPos minimalPosition = new BlockPos(zoneConstraints.minX, zoneConstraints.yLevel, zoneConstraints.minZ);
+
+        // Go through the entire farm and set the values in the matrix.
+        for (int x=0; x<zoneConstraints.lengthXAxis(); x++) {
+            for (int z=0; z<zoneConstraints.lengthZAxis(); z++) {
+                FarmSlot thisSlot = new FarmSlot();
+                thisSlot.globalBlockPos = new BlockPos(minimalPosition.getX()+x, minimalPosition.getY(), minimalPosition.getZ()+z);
+
+                Block blockAtThisSlot = mc.world.getBlockState(thisSlot.globalBlockPos).getBlock();
+                if (blockAtThisSlot instanceof SlabBlock) {
+                    thisSlot.content = FarmSlot.FarmSlotContent.COVERS_WATER;
+                }
+                else if (blockAtThisSlot instanceof AirBlock) {
+                    thisSlot.content = FarmSlot.FarmSlotContent.EMPTY;
+                }
+                else if (blockAtThisSlot instanceof MelonBlock) {
+                    thisSlot.content = FarmSlot.FarmSlotContent.MELON_BLOCK;
+                }
+                else if (blockAtThisSlot instanceof StemBlock || blockAtThisSlot instanceof StemGrownBlock) {
+                    thisSlot.content = FarmSlot.FarmSlotContent.MELON_STEM;
+                }
+                else {
+                    thisSlot.content = FarmSlot.FarmSlotContent.UNKNOWN;
+                }
+
+
+                farmSlots[x][z] = thisSlot;
+            }
+        }
 
     }
 
