a499a28e121bbf5d7abd797718a902adc59655da
inowen <inoah@correo.ugr.es>
2020-08-17 18:14:31 +0200

    Implemented PickUpItemsState
    And hopefully got the transitions from it right. Can't really test until farming pumpkins.

--- skybot/bots/melonPumpkinBot/states/PickUpItemsState.java
+++ skybot/bots/melonPumpkinBot/states/PickUpItemsState.java
@@ -2,8 +2,14 @@ package inowen.skybot.bots.melonPumpkinBot.states;
 
 import inowen.skybot.bots.melonPumpkinBot.context.MumpkinFarm;
 import inowen.skybot.hfsmBase.State;
+import inowen.utils.InventoryHelper;
+import inowen.utils.PlayerMovementHelper;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.util.math.Vec3d;
 
 public class PickUpItemsState extends State {
+
+    public static final double ANGLE_LENIENCY = 20D;
 
     public MumpkinFarm theFarm;
 
@@ -19,14 +25,44 @@ public class PickUpItemsState extends State {
     @Override
     public void run() {
 
+        // Get closest target
+        ItemEntity closestItem = theFarm.getClosestItemTo(mc.player.getPositionVector());
+
+        // Walk towards closest target (except if it's null)
+        if (closestItem != null) {
+            Vec3d closestItemPosition = closestItem.getPositionVector();
+            double yawShouldBe = PlayerMovementHelper.getYawToLookAt(closestItemPosition);
+
+            if (Math.abs(mc.player.rotationYaw - yawShouldBe) > ANGLE_LENIENCY) {
+                mc.player.rotationYaw = (float) yawShouldBe;
+            }
+
+        }
+
     }
 
     @Override
     public State getNextState() {
         State nextState = null;
+        boolean shouldTransition = theFarm.itemsToRecollect.size() == 0
+                || !InventoryHelper.isSpaceLeftToStore(theFarm.itemBeingFarmed);
+
+        if (shouldTransition) {
+            if (!InventoryHelper.isSpaceLeftToStore(theFarm.itemBeingFarmed)) {
+                nextState = new SellState(theFarm);
+            }
+            else if (theFarm.numFullyGrownBlocks() > 0) {
+                nextState = new GotoTargetState(theFarm);
+            }
+            else {
+                nextState = new WaitForGrowthState(theFarm);
+            }
+        }
 
         return nextState;
     }
+
+
 
     @Override
     public void onExit() {
