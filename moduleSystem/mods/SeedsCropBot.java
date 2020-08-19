aa24613ee7c75d41004c07699901421cc48c6dbd
inowen <inoah@correo.ugr.es>
2020-08-19 20:17:21 +0200

    Hook SeedsCropBotHFSM into module SeedsCropBot

--- moduleSystem/mods/SeedsCropBot.java
+++ moduleSystem/mods/SeedsCropBot.java
@@ -1,13 +1,84 @@
 package inowen.moduleSystem.mods;
 
 import inowen.moduleSystem.Module;
+import inowen.skybot.bots.seedsCropBot.SeedsCropBotHFSM;
 import inowen.utils.ForgeKeys;
 
 public class SeedsCropBot extends Module {
+
+    private SeedsCropBotHFSM theStateMachine;
+
+    private float lastRealYaw = 0;
+    private float lastRealPitch = 0;
 
     public SeedsCropBot() {
         super(SeedsCropBot.class.getName(), ForgeKeys.KEY_V);
     }
 
+    @Override
+    public void onEnable() {
+        theStateMachine = new SeedsCropBotHFSM();
+        theStateMachine.start();
+    }
 
+    @Override
+    public void onUpdate() {
+        // Shut off the module if there is no player to control with it.
+        if (mc.player == null) {
+            this.onDisable();
+            this.toggled = false;
+            return;
+        }
+
+        // Propagate the tick
+        if (theStateMachine != null) {
+            theStateMachine.onTick();
+        }
+
+        // Make sure there are no NaN angles.
+        fixNanAngles();
+
+        // Don't break anything that shouldn't be broken (use RayTraceHelper)
+
+    }
+
+    @Override
+    public void onGui() {
+        if (theStateMachine != null) {
+            mc.fontRenderer.drawString("Current state: " + theStateMachine.getStatePath(), 100, 100, 0xffffff);
+            // Late give some kind of information here about how many items left before sale or seeds or whatever.
+        }
+
+    }
+
+    @Override
+    public void onDisable() {
+        if (theStateMachine != null) {
+            theStateMachine.onShutdown();
+        }
+    }
+
+
+    /**
+     * Sometimes yaw / pitch become NaN for a reason that I don't know yet.
+     * This keeps track of the angles and if they become NaN, it resets to the previous one.
+     * NOTE: Might just make an event listener that does this for all bots...
+     */
+    private void fixNanAngles() {
+        // Check yaw
+        if (Float.isNaN(mc.player.rotationYaw)) {
+            mc.player.rotationYaw = lastRealYaw;
+        }
+        else {
+            lastRealYaw = mc.player.rotationYaw;
+        }
+
+        // Check pitch
+        if (Float.isNaN(mc.player.rotationPitch)) {
+            mc.player.rotationPitch = lastRealPitch;
+        }
+        else {
+            lastRealPitch = mc.player.rotationPitch;
+        }
+    }
 }
