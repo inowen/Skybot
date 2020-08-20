b6518f37fec9fc6f859a516bda2739137036e3c6
inowen <inoah@correo.ugr.es>
2020-08-20 10:14:59 +0200

    SeedCropFarm: Implemented init() and update().

--- skybot/bots/seedsCropBot/context/SeedCropFarm.java
+++ skybot/bots/seedsCropBot/context/SeedCropFarm.java
@@ -1,21 +1,27 @@
 package inowen.skybot.bots.seedsCropBot.context;
 
 import inowen.skybot.utils.FarmZoneConstraints;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 
 import java.util.ArrayList;
 
 public class SeedCropFarm {
 
     private static Minecraft mc = Minecraft.getInstance();
+    private static Block FARM_WALLS = Blocks.COBBLESTONE;
 
     private FarmZoneConstraints zoneConstraints = null;
     private FarmSlot[][] farmSlots = null;
     private ArrayList<ItemEntity> itemsToRecollect = null;
+    private SeedsCropInitTracker tracker;
 
     // Info about what is being farmed
     private Item farmedItem;
@@ -33,15 +39,27 @@ public class SeedCropFarm {
 
 
     public void init() {
+        // Get the borders (constraints) of the farm.
+        tracker = new SeedsCropInitTracker();
+        zoneConstraints = new FarmZoneConstraints(FARM_WALLS);
+        zoneConstraints.findCorners(new BlockPos(mc.player.getPositionVector().add(new Vec3d(0, 0.2, 0))), tracker);
 
+        // If a farm was found, map it out.
+        if (tracker.foundFarmConstraints) {
+            farmSlots = new FarmSlot[zoneConstraints.lengthXAxis()][zoneConstraints.lengthZAxis()];
+            updateFarmSlots();
+            updateGroundItems();
+        }
     }
 
 
 
 
     public void update() {
-
-        updateGroundItems();
+        if (tracker.foundFarmConstraints && mc.player != null) {
+            updateGroundItems();
+            updateFarmSlots();
+        }
     }
 
 
@@ -63,5 +81,14 @@ public class SeedCropFarm {
                 }
             }
         }
+    }
+
+
+    public void updateFarmSlots() {
+        if (farmSlots == null) {
+            return;
+        }
+
+
     }
 }
