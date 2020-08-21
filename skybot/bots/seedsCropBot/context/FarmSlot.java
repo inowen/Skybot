862abbc2a468433f6c9e50efaa6188b5e4f729df
inowen <inoah@correo.ugr.es>
2020-08-21 09:27:36 +0200

    SeedsCropBot: Implement FarmSlot.

--- skybot/bots/seedsCropBot/context/FarmSlot.java
+++ skybot/bots/seedsCropBot/context/FarmSlot.java
@@ -1,8 +1,27 @@
 package inowen.skybot.bots.seedsCropBot.context;
 
+import net.minecraft.util.math.BlockPos;
+
+
+/**
+ * Each one of the squares (blocks) inside the farm grid is a FarmSlot.
+ */
 public class FarmSlot {
 
+    public BlockPos globalBlockPos = null;
+    public FarmSlotContent content = null;
 
+    public FarmSlot() { }
+
+    public FarmSlot(BlockPos globalBlockPos) {
+        this.globalBlockPos = globalBlockPos;
+        this.content = FarmSlotContent.UNKNOWN;
+    }
+
+    public FarmSlot(BlockPos globalBlockPos, FarmSlotContent content) {
+        this.globalBlockPos = globalBlockPos;
+        this.content = content;
+    }
 
     public enum FarmSlotContent {
         COVERS_WATER, EMPTY_FARMLAND, GROWING, GROWN, UNKNOWN
