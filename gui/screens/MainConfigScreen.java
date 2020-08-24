81b094b9bfbefe9df05300b5c186c29a22a0b761
inowen <inoah@correo.ugr.es>
2020-08-24 20:06:49 +0200

    Added real-looking buttons to open the tabs and enclosing for testing.
    (the buttons don't do anything as of now)

--- gui/screens/MainConfigScreen.java
+++ gui/screens/MainConfigScreen.java
@@ -9,15 +9,21 @@ import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.client.gui.widget.button.CheckboxButton;
 import net.minecraft.util.text.ITextComponent;
 
+
+/**
+ * The main settings screen. Checkboxes to enable/disable modules, and "tabs" to choose the right settings screen
+ * to put in the center of the main screen.
+ * To create the different kinds of tabs in the middle while still being able to use the exit button and the
+ * checkboxes, each tab is actually its own gui that inherits from the MainConfigScreen and adds something.
+ */
 public class MainConfigScreen extends Screen {
 
     protected Minecraft mc = Minecraft.getInstance();
 
-    // Widgets: Checkbox buttons
+    // Widgets: Checkbox buttons (need to be class-wide to access .isChecked())
     private CheckboxButton hideNamesCheckBox = null;
     private CheckboxButton whitelistCheckbox = null;
-    public int topModCheckboxY;
-    public int modCheckboxX;
+
 
     // Widgets: Tabs buttons (settings for one or the other bot).
     // These lead to another identical gui screen except that there are settings for the chosen bot in the center.
@@ -34,9 +40,38 @@ public class MainConfigScreen extends Screen {
         super.init();
 
         // Create and add the checkboxButtons to toggle the modules.
-        modCheckboxX = (int)(0.025*this.width);
-        topModCheckboxY = (int)(0.1*this.height);
+        int modCheckboxX = (int)(0.025*this.width);
+        int topModCheckboxY = (int)(0.1*this.height);
         createAndAddModCheckboxes(modCheckboxX, topModCheckboxY);
+
+        // Create and add the buttons to open the different options tabs.
+        int tabsButtonsHeight = (int)(0.09*this.height);
+        int tabsButtonsXOffset = (int)(0.175*this.width);
+        int buttonWidth = 82;
+        this.addButton(new Button(tabsButtonsXOffset, tabsButtonsHeight, buttonWidth, 20, "CropBot", button -> {
+            System.out.println("TestButtonPressed");
+        }));
+        this.addButton(new Button(tabsButtonsXOffset+buttonWidth*1, tabsButtonsHeight, buttonWidth, 20, "SugarcaneBot", button -> {
+            System.out.println("TestButtonPressed");
+        }));
+        this.addButton(new Button(tabsButtonsXOffset+buttonWidth*2, tabsButtonsHeight, buttonWidth, 20, "MelonPumpkinBot", button -> {
+            System.out.println("TestButtonPressed");
+        }));
+        this.addButton(new Button(tabsButtonsXOffset+buttonWidth*3, tabsButtonsHeight, buttonWidth, 20, "SeedsCropBot", button -> {
+            System.out.println("TestButtonPressed");
+        }));
+        this.addButton(new Button(tabsButtonsXOffset + buttonWidth*4, tabsButtonsHeight, buttonWidth, 20, "NetherwartsBot", button -> {
+            System.out.println("TestButtonPressed");
+        }));
+        this.addButton(new Button(tabsButtonsXOffset + buttonWidth*5, tabsButtonsHeight, buttonWidth, 20, "ChorusBot", button -> {
+            System.out.println("TestButtonPressed");
+        }));
+
+        // Testing how it would look, surround the area with the settings for each bot with long button-lines.
+        this.addButton(new Button(tabsButtonsXOffset, tabsButtonsHeight + 30, 5, 500, "", button -> {}));
+        this.addButton(new Button(tabsButtonsXOffset, tabsButtonsHeight + 30, 1000, 5, "", button -> {}));
+        this.addButton(new Button(this.width-10, tabsButtonsHeight + 30, 5, 500, "", button -> {}));
+
 
         // Create and add the exit button
         this.addButton(new Button((int)(0.75*this.width), (int)(0.9*this.height), 100, 20, "Exit", button -> {mc.displayGuiScreen(null);}));
@@ -86,12 +121,12 @@ public class MainConfigScreen extends Screen {
         int numModCheckBoxes = 0;
 
         // Checkbox for HideNames module
-        hideNamesCheckBox = new CheckboxButton(modCheckboxX, topModCheckboxY + numModCheckBoxes*20, 20, 20, "Hide Names", ModuleManager.getModule("HideNames").isToggled());
+        hideNamesCheckBox = new CheckboxButton(x, topY + numModCheckBoxes*20, 20, 20, "Hide Names", ModuleManager.getModule("HideNames").isToggled());
         this.addButton(hideNamesCheckBox);
         numModCheckBoxes++;
 
         // Checkbox for Whitelist module
-        whitelistCheckbox = new CheckboxButton(modCheckboxX, topModCheckboxY + numModCheckBoxes*20, 20,20, "WhiteList", ModuleManager.getModule("WhiteList").isToggled());
+        whitelistCheckbox = new CheckboxButton(x, topY + numModCheckBoxes*20, 20,20, "WhiteList", ModuleManager.getModule("WhiteList").isToggled());
         this.addButton(whitelistCheckbox);
         numModCheckBoxes++;
 
