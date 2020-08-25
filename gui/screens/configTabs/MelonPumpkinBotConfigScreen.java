0c33ff313ea8bb997feb6a093f0727c4fbfb64b2
inowen <inoah@correo.ugr.es>
2020-08-25 18:02:21 +0200

    Extend MainConfigScreen from the settings tabs screens.
    Override functions, create structure.

--- gui/screens/configTabs/MelonPumpkinBotConfigScreen.java
+++ gui/screens/configTabs/MelonPumpkinBotConfigScreen.java
@@ -1,4 +1,31 @@
 package inowen.gui.screens.configTabs;
 
-public class MelonPumpkinBotConfigScreen {
+import inowen.gui.screens.MainConfigScreen;
+import net.minecraft.util.text.ITextComponent;
+
+public class MelonPumpkinBotConfigScreen extends MainConfigScreen {
+
+    /**
+     * Constructor.
+     *
+     * @param titleIn Title for the window (can be accessed through this.title)
+     */
+    public MelonPumpkinBotConfigScreen(ITextComponent titleIn) {
+        super(titleIn);
+    }
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    @Override
+    public void render(int mouseX, int mouseY, float partialTicks) {
+        super.render(mouseX, mouseY, partialTicks);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+    }
 }
--- gui/screens/configTabs/NetherwartBotConfigScreen.java
+++ gui/screens/configTabs/NetherwartBotConfigScreen.java
@@ -1,4 +1,30 @@
 package inowen.gui.screens.configTabs;
 
-public class NetherwartBotConfigScreen {
+import inowen.gui.screens.MainConfigScreen;
+import net.minecraft.util.text.ITextComponent;
+
+public class NetherwartBotConfigScreen extends MainConfigScreen {
+
+    /**
+     * Constructor.
+     * @param titleIn Title for the window (can be accessed through this.title)
+     */
+    public NetherwartBotConfigScreen(ITextComponent titleIn) {
+        super(titleIn);
+    }
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    @Override
+    public void render(int mouseX, int mouseY, float partialTicks) {
+        super.render(mouseX, mouseY, partialTicks);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+    }
 }
