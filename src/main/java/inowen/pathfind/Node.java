8ce8508398b8e6a74f27920409d0cfbbdc32f5d6
inowen <inoah@correo.ugr.es>
2020-08-27 13:00:33 +0200

    Pathfind: Add visited (bool) and dist to origin to Node for algorithm

--- src/main/java/inowen/pathfind/Node.java
+++ src/main/java/inowen/pathfind/Node.java
@@ -4,36 +4,60 @@ import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 
 public class Node {
+    // Big enough number to be considered infinite in this context
+    public static final double INFINITY = 999999D;
+
     public BlockPos globalPosition;
     public BlockState blockState;
+    public boolean visited = false;
+    public double distanceToOrigin = INFINITY;
 
     public Node() {
         globalPosition = null;
         blockState = null;
+        visited = false;
     }
 
     public Node(BlockPos pos) {
         globalPosition = pos;
+        blockState = null;
+        visited = false;
     }
 
     public Node(BlockState bs) {
         blockState = bs;
+        globalPosition = null;
+        visited = false;
     }
 
     public Node(BlockState bs, BlockPos bp) {
         blockState = bs;
         globalPosition = bp;
+        visited = false;
     }
 
+    public boolean wasVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
 
     public BlockPos getGlobalPosition() {
         return globalPosition;
     }
 
-
     public BlockState getBlockState() {
         return blockState;
     }
 
+    public double getDistToOrigin() {
+        return distanceToOrigin;
+    }
+
+    public void setDistToOrigin(double dist) {
+        this.distanceToOrigin = dist;
+    }
 
 }
