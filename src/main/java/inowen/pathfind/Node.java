fc95f69a9320ebb592f6281789df12ac3b6356a3
inowen <inoah@correo.ugr.es>
2020-08-27 09:35:16 +0200

    Pathfind: Implemented Node

--- src/main/java/inowen/pathfind/Node.java
+++ src/main/java/inowen/pathfind/Node.java
@@ -1,4 +1,39 @@
 package inowen.pathfind;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+
 public class Node {
+    public BlockPos globalPosition;
+    public BlockState blockState;
+
+    public Node() {
+        globalPosition = null;
+        blockState = null;
+    }
+
+    public Node(BlockPos pos) {
+        globalPosition = pos;
+    }
+
+    public Node(BlockState bs) {
+        blockState = bs;
+    }
+
+    public Node(BlockState bs, BlockPos bp) {
+        blockState = bs;
+        globalPosition = bp;
+    }
+
+
+    public BlockPos getGlobalPosition() {
+        return globalPosition;
+    }
+
+
+    public BlockState getBlockState() {
+        return blockState;
+    }
+
+
 }
