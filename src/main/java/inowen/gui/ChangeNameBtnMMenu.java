592ca1460ce322772b1a18e83420277ea7184d8b
inowen <inoah@correo.ugr.es>
2020-08-29 17:59:10 +0200

    Create button in main menu to open Session Change screen.

---
+++ src/main/java/inowen/gui/ChangeNameBtnMMenu.java
@@ -0,0 +1,31 @@
+package inowen.gui;
+
+import inowen.SkyBotMod;
+import net.minecraft.client.gui.screen.MainMenuScreen;
+import net.minecraft.client.gui.widget.button.Button;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Adds a button to the main menu that leads to the screen where
+ * the user can change their Session.username.
+ */
+
+@Mod.EventBusSubscriber(modid= SkyBotMod.MOD_ID, value= Dist.CLIENT)
+public class ChangeNameBtnMMenu {
+
+    @SubscribeEvent
+    public static void buttonAdder(GuiScreenEvent.InitGuiEvent event) {
+        if (event.getGui() instanceof MainMenuScreen) {
+            int x = (int)(0.02*event.getGui().width);
+            int y = (int)(0.72*event.getGui().height);
+            event.addWidget(new Button(x, y, 85, 20, "Change session", button -> {
+                // When the button is pressed
+
+            }));
+        }
+    }
+
+}
