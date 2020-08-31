5ddd2952eb4e9ae2a22e1433d91912ff56938abd
inowen <inoah@correo.ugr.es>
2020-08-31 10:59:21 +0200

    Added utility to work with RGB colors

---
+++ src/main/java/inowen/utils/Colors.java
@@ -0,0 +1,13 @@
+package inowen.utils;
+
+public class Colors {
+
+    public static final int RED = 0xff0000;
+    public static final int DARK_RED = 0x990000;
+
+    public static final int BLUE = 0x0000ff;
+    public static final int DARK_BLUE = 0x000099;
+
+    public static final int GREEN = 0x00ff00;
+    public static final int DARK_GREEN = 0x009900;
+}
