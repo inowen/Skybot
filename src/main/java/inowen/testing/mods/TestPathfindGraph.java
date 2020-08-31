1fabb0e191baafec888089bf137fb271f6cba2f1
inowen <inoah@correo.ugr.es>
2020-08-31 11:53:32 +0200

    Pathfind: Add testing for adjacency checker between nodes.
    Seems to work fine so far.

--- src/main/java/inowen/testing/mods/TestPathfindGraph.java
+++ src/main/java/inowen/testing/mods/TestPathfindGraph.java
@@ -2,11 +2,13 @@ package inowen.testing.mods;
 
 import inowen.moduleSystem.Module;
 import inowen.pathfind.Graph;
+import inowen.pathfind.Node;
 import inowen.utils.ForgeKeys;
 
 public class TestPathfindGraph extends Module {
 
     public Graph theGraph = null;
+    public int numNeighbors = 0;
 
     public TestPathfindGraph() {
         super("TestPathfindGraph", ForgeKeys.KEY_G);
@@ -15,6 +17,13 @@ public class TestPathfindGraph extends Module {
     @Override
     public void onEnable() {
         theGraph = new Graph(mc.player.getPosition(), 50, mc.world);
+        // Test adjacency
+        theGraph.calcNeighbours(mc.world);
+        //... count total number of neighbours.
+        numNeighbors = 0;
+        for (Node node : theGraph.nodes) {
+            numNeighbors += node.neighbours.size();
+        }
     }
 
     @Override
@@ -26,6 +35,8 @@ public class TestPathfindGraph extends Module {
     public void onGui() {
         if (theGraph != null) {
             mc.fontRenderer.drawString("Nodes in the graph: " + theGraph.nodes.size(), 50, 50, 0xffffff);
+            mc.fontRenderer.drawString("Average amount of neighbors: " + (double)numNeighbors/theGraph.nodes.size(), 50, 60, 0xffffff);
+            mc.fontRenderer.drawString("Edges in the graph: " + (double)numNeighbors/2, 50, 70, 0xffffff);
         }
     }
 
