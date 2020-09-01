8b8e3d512455c4c2f85d83c463bfff1305a4683f
inowen <inoah@correo.ugr.es>
2020-09-01 18:05:53 +0200

    Config: Add class to represent options.

---
+++ src/main/java/inowen/config/ConfigOption.java
@@ -0,0 +1,34 @@
+package inowen.config;
+
+
+/**
+ * With T as Integer, Boolean, String or Double.
+ * @param <T>
+ */
+public class ConfigOption<T> {
+
+    public String name = "";
+    public T value = null;
+    public T defaultValue = null;
+
+    public ConfigOption(String name, T value, T defaultValue) {
+        this.name = name;
+        this.value = value;
+        this.defaultValue = defaultValue;
+    }
+
+    /**
+     * Sets Integer, Boolean, String or Double.
+     * @param asString
+     */
+    public void setValue(String asString) {
+        if ((value instanceof Boolean) || (defaultValue instanceof Boolean)) {
+            value = (T)Boolean.valueOf(asString);
+        }
+        else {
+            value = (T)asString;
+        }
+    }
+
+
+}
