1a9aa5a3358254829e9a51d0e02fc9553104a4e2
inowen <inoah@correo.ugr.es>
2020-09-01 19:13:45 +0200

    Config: Function to collect all ConfigOption fields in SkybotConfig class

--- src/main/java/inowen/config/SkybotConfig.java
+++ src/main/java/inowen/config/SkybotConfig.java
@@ -1,11 +1,13 @@
 package inowen.config;
 
 
+import inowen.SkyBotMod;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 
@@ -70,6 +72,41 @@ public class SkybotConfig {
 
     // ------------ HIDE NAMES MODULE ------------------
     public static ConfigOption<String> HIDE_NAMES_SUBSTITUTE = new ConfigOption<>("Hide_Names_Substitute", "Skybot_Hidden_Name", "Skybot_Hidden_Name");
+
+
+
+
+
+
+
+    // -----------------------------------------------------------------
+    // -----------------------------------------------------------------
+    // -----------------  For Saving / Reading Options -----------------
+    // -----------------------------------------------------------------
+    // -----------------------------------------------------------------
+
+    /**
+     * Get a list of all the ConfigOption in the fields of this class.
+     * @return
+     */
+    public static ArrayList<Object> getConfigOptions() {
+        ArrayList<Object> oList = new ArrayList<>();
+
+        // Get all fields that contain a ConfigOption
+        try {
+            Field[] fields = SkybotConfig.class.getFields();
+            for (Field field : fields) {
+                if (field.get(instance) instanceof ConfigOption) {
+                    oList.add(field.get(instance));
+                }
+            }
+        }
+        catch (Exception e) {
+            SkyBotMod.LOGGER.error("Error reading config options from fields in SkybotConfig class.");
+        }
+
+        return oList;
+    }
 
 
 }
