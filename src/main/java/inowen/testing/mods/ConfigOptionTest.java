0ffeb3e109a108c3d590bff164749c45ab664778
inowen <inoah@correo.ugr.es>
2020-09-02 22:10:08 +0200

    Testing: Check File class, creating and deleting files.

--- src/main/java/inowen/testing/mods/ConfigOptionTest.java
+++ src/main/java/inowen/testing/mods/ConfigOptionTest.java
@@ -4,7 +4,9 @@ import inowen.config.ConfigOption;
 import inowen.config.SkybotConfig;
 import inowen.moduleSystem.Module;
 import inowen.utils.ForgeKeys;
-import sun.security.krb5.Config;
+import net.minecraftforge.fml.loading.FMLPaths;
+
+import java.io.*;
 
 public class ConfigOptionTest extends Module {
 
@@ -16,6 +18,28 @@ public class ConfigOptionTest extends Module {
      @Override
     public void onEnable() {
          System.out.println("---------------------------------------");
+
+         File configFolder = FMLPaths.CONFIGDIR.get().toFile();
+         File textFile = new File(configFolder, "something.txt");
+         File textFile2 = new File(configFolder, "newFile.txt");
+
+         try {
+             textFile2.createNewFile();
+             textFile2.delete();
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+
+         try {
+             FileReader reader = new FileReader(textFile);
+             BufferedReader breader = new BufferedReader(reader);
+             while(breader.ready()) {
+                 System.out.println(breader.readLine());
+             }
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
+
          System.out.println("---------------------------------------");
 
         System.out.println("Config fields in SkybotConfig:");
@@ -25,6 +49,12 @@ public class ConfigOptionTest extends Module {
 
 
         System.out.println("---------------------------------------");
+
+        System.out.println("Configs detected, as strings: ");
+        for (String s : SkybotConfig.getConfigsAsStrings()) {
+            System.out.println("\t\t" + s);
+        }
+
         System.out.println("---------------------------------------");
      }
 }
