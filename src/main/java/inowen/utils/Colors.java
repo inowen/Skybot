a80846151e0b7cfe91dd0b7466c2380adacb6f38
inowen <inoah@correo.ugr.es>
2020-09-02 10:56:35 +0200

    Utils->Colors: Add RGB colors

--- src/main/java/inowen/utils/Colors.java
+++ src/main/java/inowen/utils/Colors.java
@@ -7,7 +7,12 @@ public class Colors {
 
     public static final int BLUE = 0x0000ff;
     public static final int DARK_BLUE = 0x000099;
+    public static final int LIGHT_BLUE = 0x3399ff; // (blue + green)
 
     public static final int GREEN = 0x00ff00;
     public static final int DARK_GREEN = 0x009900;
+
+    public static final int WHITE = 0xffffff;
+    public static final int BLACK = 0;
+
 }
