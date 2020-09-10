7b16f0b84f7fe76de3f0052a8acc2457365d25b7
inowen <inoah@correo.ugr.es>
2020-09-10 21:26:13 +0200

    Utils: reduce minecraft.point.paths to only the last word

--- src/main/java/inowen/utils/StringFormatter.java
+++ src/main/java/inowen/utils/StringFormatter.java
@@ -1,4 +1,22 @@
 package inowen.utils;
 
 public class StringFormatter {
+
+    /**
+     * Process strings like the following: "minecraft.effects.strength" to return
+     * only the part after the last point. If there are no points, the same string is returned.
+     * @param original
+     * @return String
+     */
+    public static String removePointPath(String original) {
+        String result = "";
+        int readPos = original.length()-1;
+
+        while(readPos>=0 && original.charAt(readPos)!='.') {
+            result.concat(String.valueOf(original.charAt(readPos)));
+            readPos--;
+        }
+
+        return result;
+    }
 }
