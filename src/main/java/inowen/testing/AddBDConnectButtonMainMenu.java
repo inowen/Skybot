fe91e567ab36e9b4ee3b0e0bb9fcd991c0cb3760
inowen <inoah@correo.ugr.es>
2020-10-05 19:23:05 +0200

    Testing: Get server login test to work

--- src/main/java/inowen/testing/AddBDConnectButtonMainMenu.java
+++ src/main/java/inowen/testing/AddBDConnectButtonMainMenu.java
@@ -1,20 +1,34 @@
 package inowen.testing;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.ConnectingScreen;
 import net.minecraft.client.gui.screen.MainMenuScreen;
+import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.client.multiplayer.ServerData;
 import net.minecraftforge.client.event.GuiOpenEvent;
+import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
 public class AddBDConnectButtonMainMenu {
 
+    private static final Minecraft mc = Minecraft.getInstance();
+
     private static final String BD_ADDRESS = "mc.blockdrop.org";
 
     @SubscribeEvent
-    public static void addButton(GuiOpenEvent event) {
+    public static void addButton(GuiScreenEvent.InitGuiEvent event) {
         if (event.getGui() instanceof MainMenuScreen) {
-            // Right now just test if I'm getting the right gui. Later actually add button.
-            System.out.println("Opened a main menu screen!");
+            event.addWidget(new Button(0, 0, 100, 20, "New button", button -> connectServer(BD_ADDRESS)));
         }
+    }
+
+
+    private static void connectServer(String ip) {
+        ServerData serverData = new ServerData("BlockDrop", ip, false);
+        mc.displayGuiScreen(new ConnectingScreen(mc.currentScreen, mc, serverData));
+
+
     }
 }
