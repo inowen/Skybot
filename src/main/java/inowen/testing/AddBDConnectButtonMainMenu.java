2382ce236e982241b2a4f1d7693f8ba602dd583e
inowen <inoah@correo.ugr.es>
2020-10-05 19:37:35 +0200

    Testing: Add event subscriber to disconnect on entering inv.
    Also comment this subscriber and the one that adds the login button in main menu

--- src/main/java/inowen/testing/AddBDConnectButtonMainMenu.java
+++ src/main/java/inowen/testing/AddBDConnectButtonMainMenu.java
@@ -3,6 +3,7 @@ package inowen.testing;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.ConnectingScreen;
 import net.minecraft.client.gui.screen.MainMenuScreen;
+import net.minecraft.client.gui.screen.inventory.InventoryScreen;
 import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.client.multiplayer.ServerData;
 import net.minecraftforge.client.event.GuiOpenEvent;
@@ -17,10 +18,17 @@ public class AddBDConnectButtonMainMenu {
 
     private static final String BD_ADDRESS = "mc.blockdrop.org";
 
-    @SubscribeEvent
+    // @SubscribeEvent
     public static void addButton(GuiScreenEvent.InitGuiEvent event) {
         if (event.getGui() instanceof MainMenuScreen) {
             event.addWidget(new Button(0, 0, 100, 20, "New button", button -> connectServer(BD_ADDRESS)));
+        }
+    }
+
+    // @SubscribeEvent
+    public static void disconnectAtOpeningInventory(GuiScreenEvent.InitGuiEvent event) {
+        if (event.getGui() instanceof InventoryScreen) {
+            mc.world.sendQuittingDisconnectingPacket();
         }
     }
 
@@ -28,7 +36,5 @@ public class AddBDConnectButtonMainMenu {
     private static void connectServer(String ip) {
         ServerData serverData = new ServerData("BlockDrop", ip, false);
         mc.displayGuiScreen(new ConnectingScreen(mc.currentScreen, mc, serverData));
-
-
     }
 }
