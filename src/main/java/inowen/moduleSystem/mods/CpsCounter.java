12c7876b765ca730a6630ab31aa6e15a154eda7f
inowen <inoah@correo.ugr.es>
2020-10-18 23:04:20 +0200

    Mods: Added mod to count cps.

---
+++ src/main/java/inowen/moduleSystem/mods/CpsCounter.java
@@ -0,0 +1,16 @@
+package inowen.moduleSystem.mods;
+
+import inowen.SkyBotMod;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Everything needed to intercept the clicks sent to the game, count the average CPS
+ * and draw them to the screen.
+ *
+ * Idea: Report clicks done in the last second, and average cps the last 5 seconds.
+ */
+@Mod.EventBusSubscriber(modid= SkyBotMod.MOD_ID, value= Dist.CLIENT)
+public class CpsCounter {
+
+}
