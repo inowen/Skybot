00f0cc8211c072069cd16197cc73d6bcdff27e32
inowen <inoah@correo.ugr.es>
2020-10-18 23:45:21 +0200

    CpsCounter: Event handlers added
    To catch mouse clicking events, and to display something on the screen

--- src/main/java/inowen/moduleSystem/mods/CpsCounter.java
+++ src/main/java/inowen/moduleSystem/mods/CpsCounter.java
@@ -4,6 +4,9 @@ import inowen.SkyBotMod;
 import inowen.moduleSystem.Module;
 import inowen.utils.ForgeKeys;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 /**
@@ -17,5 +20,17 @@ public class CpsCounter extends Module {
 
     public CpsCounter() {
         super("CpsCounter", ForgeKeys.KEY_NONE);
+    }
+
+
+    @SubscribeEvent
+    public static void getMousePress(InputEvent.MouseInputEvent event) {
+        System.out.println("Mouse input event. Button: " + event.getButton()); // Button 0 is down. Divide by 2 (one event is mouse up, another is down).
+    }
+
+
+    @SubscribeEvent
+    public static void showOnScreen(RenderGameOverlayEvent event) {
+
     }
 }
