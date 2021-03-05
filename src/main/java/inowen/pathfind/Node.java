292b5e47c13ddaf74454bde5425ffbc768e15056
inowen <inoah@correo.ugr.es>
2021-03-05 21:29:22 +0100

    Pathfinding: Node for A* algorithm.

--- src/main/java/inowen/pathfind/Node.java
+++ src/main/java/inowen/pathfind/Node.java
@@ -1,4 +1,23 @@
 package inowen.pathfind;
 
+import net.minecraft.util.math.BlockPos;
+
 public class Node {
+
+    private BlockPos position;
+    private double cost = 0;
+
+    /**
+     * Node in the A* algorithm
+     * @param position
+     * @param cost
+     */
+    public Node(BlockPos position, double cost) {
+        this.position = position;
+        this.cost = cost;
+    }
+
+    public BlockPos getPosition() { return position; }
+
+    public double getCost() { return cost; }
 }
