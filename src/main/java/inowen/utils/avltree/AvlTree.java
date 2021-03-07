4fbbfee1f869eacece1b91432c80a53e69cc4132
inowen <inoah@correo.ugr.es>
2021-03-07 21:52:44 +0100

    Change doc for AvlTree

--- src/main/java/inowen/utils/avltree/AvlTree.java
+++ src/main/java/inowen/utils/avltree/AvlTree.java
@@ -2,7 +2,15 @@ package inowen.utils.avltree;
 
 /**
  * AVL-Tree.
- * O(log(n)) insertion, deletion, retrieval.
+ * O(log2(n)) insertion, deletion, retrieval.
+ *
+ * Returns references to contained AvlNodes. Since the Avl-Tree is an ordered structure,
+ * those values shouldn't be changed directly on the node inside the tree.
+ *
+ * In order to change something in the tree, first delete the corresponding node,
+ * then change it, after that reinsert it.
+ *
+ * @author Noah Wenck (inowen)
  */
 public class AvlTree<T> {
 
