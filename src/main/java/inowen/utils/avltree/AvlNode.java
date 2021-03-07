d4f3dc7fe30f0929f0f5cb2e62adfa6b0d1155bf
inowen <inoah@correo.ugr.es>
2021-03-07 21:40:36 +0100

    Document AVL Node

--- src/main/java/inowen/utils/avltree/AvlNode.java
+++ src/main/java/inowen/utils/avltree/AvlNode.java
@@ -2,23 +2,60 @@ package inowen.utils.avltree;
 
 public class AvlNode<T> {
 
-    private T value;
+    private T value = null;
     private AvlNode<T> parent = null;
     private AvlNode<T> left = null;
     private AvlNode<T> right = null;
 
+    /**
+     * Returns the parent of this node.
+     * @return Parent
+     */
     public AvlNode<T> parent() {
         return parent;
     }
 
+    /**
+     * Returns the left child of this node.
+     * @return Left child
+     */
     public AvlNode<T> left() {
         return left;
     }
 
+    /**
+     * Returns the right child of this node.
+     * @return Right child
+     */
     public AvlNode<T> right() {
         return right;
     }
 
+    /**
+     * Returns the value that this node contains.
+     * @return
+     */
+    public T getValue() {
+        return value;
+    }
 
+    /**
+     * Whether this node is a null node (contains nothing, empty, not a node).
+     * @return Boolean
+     */
+    public boolean isNull() {
+        return value == null;
+    }
+
+
+    /**
+     * Height of this node from the bottom of the tree.
+     * Length of the longest path to a leaf.
+     * Leaf heights are 0, null nodes would have -1 height.
+     * @return
+     */
+    private int height() {
+
+    }
 
 }
